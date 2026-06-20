173e6eee806ae3bbf94207b70673f4ceac2d9533
najwayu22 <2410010358@uniska-bjm.ac.id>
2026-06-21 07:17:11 +0800

    Selesai Bagian 3: Multi-Class dengan Berkas

---
+++ src/bagian3/kontak/MainKontak.java
@@ -0,0 +1,27 @@
+package bagian3.kontak;
+
+public class MainKontak {
+    public static void main(String[] args) {
+        // Membuat objek pengelola dan mengisinya
+        BukuKontak buku = new BukuKontak("kontak.txt");
+        buku.tambahKontak(new Kontak("Andi", "0811111", "andi@gmail.com"));
+        buku.tambahKontak(new Kontak("Budi", "0822222", "budi@gmail.com"));
+        buku.tambahKontak(new Kontak("Citra", "0833333", "citra@gmail.com"));
+        
+        buku.tampilkanSemua();
+        buku.simpanKeBerkas();
+        
+        System.out.println();
+        
+        // Objek baru yang kosong, lalu memuat dari berkas
+        BukuKontak bukuLain = new BukuKontak("kontak.txt");
+        bukuLain.muatDariBerkas();
+        bukuLain.tampilkanSemua();
+        System.out.println("Jumlah kontak: " + bukuLain.jumlahKontak());
+        
+        buku.cariKontak("awa");
+        buku.cariKontak("Budi");
+        
+        buku.hapusKontak("Andi");
+    }
+}
